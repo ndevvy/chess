9d9b0221dd408b00e8a9a4fae270f21a655f044a
App Academy Student <student@appacademy.io>
2015-08-31 19:47:06 -0700

    add chess pieces to board

--- board.rb
+++ board.rb
@@ -1,6 +1,6 @@
 require_relative 'piece.rb'
-class Board
 
+class Board
   attr_accessor :grid
 
   def initialize
@@ -14,6 +14,48 @@ class Board
     # raise BadMoveError.new, "can't move there" unless self[start].valid_move(end_pos)?
     self[start] = EmptySquare.new
     #return true or false to PLayer depending on if move succeeded
+  end
+
+  def setup
+    # if color == white
+    #   board = self.reverse
+    # else
+    #   board = self
+    # end
+
+    grid[1].each_with_index do |piece, index| # this doesn't work yet
+      piece = Pawn.new(:black, [1,index], self)
+    end
+
+    grid[6].each_with_index do |piece, index| # ditto
+      piece = Pawn.new(:white, [1, index], self)
+    end
+
+    place_piece(:white, [0,0], Rook)
+    place_piece(:white, [0,7], Rook)
+    place_piece(:white, [0,1], Knight)
+    place_piece(:white, [0,6], Knight)
+    place_piece(:white, [0,2], Bishop)
+    place_piece(:white, [0,5], Bishop)
+    place_piece(:white, [0,3], Queen)
+
+    place_piece(:black, [6,0], Rook)
+    place_piece(:black, [6,7], Rook)
+    place_piece(:black, [6,1], Knight)
+    place_piece(:black, [6,6], Knight)
+    place_piece(:black, [6,2], Bishop)
+    place_piece(:black, [6,5], Bishop)
+    place_piece(:black, [6,3], Queen)
+
+
+  end
+
+  def place_piece(color, pos, piece)
+    self[pos] = piece.new(color, pos, self)
+  end
+
+
+  def dup # duplicate board and its pieces
   end
 
   def [](pos)
